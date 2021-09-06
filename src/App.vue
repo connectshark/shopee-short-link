75c40b60063b342c0bd4fd326758339a6c7ae8a6
connectshark <bobhus394@gmail.com>
2021-09-06 12:02:44 +0800

    新增字體

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,14 @@
 
 <script>
 export default {
-  name: 'App',
-  components: {
-  }
+  name: 'App'
 }
 </script>
 
 <style>
+@import 'assets/css/font.css';
 #app {
-  font-family: 'Noto Sans TC', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-family: 'openhuninn', 'Noto Sans TC', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }

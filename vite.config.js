b513eaebd36576a57157089313d75fe94299511d
connectshark <bobhus394@gmail.com>
2021-10-15 14:17:07 +0800

    改用render放置網站位置

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,5 @@ export default defineConfig({
   plugins: [vue(), VitePWA({})],
   server: {
     port: 8080
-  },
-  base: process.env.NODE_ENV === 'production'
-    ? '/shopee-short-link/'
-    : '/',
+  }
 })

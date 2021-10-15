29a270ccbb482d5512110d684aa2feed98d4d03b
connectshark <bobhus394@gmail.com>
2021-10-15 14:59:57 +0800

    新增pwa設定

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,33 @@ import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [vue(), VitePWA({})],
+  plugins: [vue(), VitePWA({
+    includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+    manifest: {
+      name: '蝦皮短網址服務',
+      short_name: '縮蝦',
+      description: '縮短所有蝦皮購物內的網址，同樣具有預覽連結和網址',
+      theme_color: '#bf4800',
+      icons: [
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable',
+        }
+      ]
+    }
+  })],
   server: {
     port: 8080
   }

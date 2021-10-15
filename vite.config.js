f27a77a49cc371647dc3ba4179d1f99ebf24d861
connectshark <bobhus394@gmail.com>
2021-10-15 15:25:54 +0800

    新增開始網址

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,15 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [vue(), VitePWA({
+    base: '/',
     includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
     manifest: {
       name: '蝦皮短網址服務',
       short_name: '縮蝦',
       description: '縮短所有蝦皮購物內的網址，同樣具有預覽連結和網址',
       theme_color: '#bf4800',
+      start_url: '/',
+      lang: 'zh-Hant-TW',
       icons: [
         {
           src: 'pwa-192x192.png',

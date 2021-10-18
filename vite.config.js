4d607065186b12be402f58c7daf85afa982c84c6
connectshark <bobhus394@gmail.com>
2021-10-18 12:30:21 +0800

    棄用pwa

--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
-    vue(),
-    VitePWA({
-      base: '/',
-      includeAssets: ['favicon.svg'],
-      manifest: {
-        name: 'PWA Basic',
-        short_name: 'PWA Basic',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: 'pwa-192x192.png', // <== don't add slash, for testing
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png', // <== don't remove slash, for testing
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png', // <== don't add slash, for testing
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
-        ],
-      },
-    }),
+    vue()
   ],
   server: {
     port: 8080

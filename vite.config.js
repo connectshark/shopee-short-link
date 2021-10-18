1e0954837d04a362abfc1dadb6ace8754f2f184a
connectshark <bobhus394@gmail.com>
2021-10-18 12:00:47 +0800

    Revert "棄用pwa"
    This reverts commit b4636c0a6c6a0521c730c3bfa7f1a8ffd3471abb.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,38 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      base: '/',
+      includeAssets: ['favicon.png'],
+      workbox: {
+        cleanupOutdatedCaches: true,
+        skipWaiting: true,
+        clientsClaim: true
+      },
+      manifest: {
+        name: 'V2P2 Starter',
+        short_name: 'V2P2',
+        background_color: '#acafb6',
+        theme_color: '#acafb6',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          }
+        ]
+      }
+    })
+  ],
   server: {
     port: 8080
   }

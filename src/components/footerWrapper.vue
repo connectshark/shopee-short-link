ffdec839d071c0a9deba5daa24c2fe37b0ac7827
connectshark <bobhus394@gmail.com>
2022-01-20 18:06:57 +0800

    微調footer樣式

--- src/components/footerWrapper.vue
+++ src/components/footerWrapper.vue
@@ -3,15 +3,15 @@ const copyright = `Copyright©2022 All Rights Reserved.`
 </script>
 
 <template>
-  <footer class="bg-gray-800 text-white py-2 text-center">
-    <p class="text-sm max-w-3xl mx-auto">{{ copyright }}</p>
+  <footer class="bg-gray-800 text-white py-2 text-center text-sm">
+    <p class="max-w-3xl mx-auto">{{ copyright }}</p>
     <p class="max-w-3xl mx-auto">
       <a
         href="mailto:bobhus394@gmail.com"
         class="inline-block transition hover:underline active:underline mx-1.5 active:scale-110"
         title="聯繫"
         target="_blank"
-      >chambers
+      >Chambers
         <i class="bx bx-mail-send"></i>
       </a>
     </p>

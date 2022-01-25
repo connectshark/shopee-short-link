28edde9366014f1f06c5476ff434a003a2984355
connectshark <bobhus394@gmail.com>
2022-01-25 15:27:08 +0800

    使用ga插件

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import { fileURLToPath } from 'url'
+import ViteRadar from 'vite-plugin-radar'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    ViteRadar({
+      analytics: {
+        id: 'G-J6P3NCP3ZW',
+      },
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
